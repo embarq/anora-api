d17d9ccfa3cb3d0331fe4791da0cec4b51677627
embarq <embarq.dev@gmail.com>
2017-04-16 23:23:57 +0300

    Implement password hashing. Start to implement validation erorrs handling

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,25 @@
 import { RequestHandler } from 'express';
 import { strictEqual } from 'assert';
 import { Connection, Document, Model } from 'mongoose';
+import { compare, hash } from 'bcryptjs';
+
 import { getUserModel, Credentials, User } from '../user/user.model';
+import { ControllerMethods } from '../etc/declarations';
 import { baseRequestHandler } from '../etc/helpers';
+
+/**
+ * @private
+ */
+const registerUser = (model: Model<Document>) =>
+  (user: User) => hash(user.password, 8)
+    .then(hash => model.create(Object.assign(user, { password: hash })));
+
+const userDocumentToObject = (doc: Document) => doc.toObject({
+  minimize: true,
+  transform: (doc, ret) => {
+    delete (ret as any).password;
+  }
+})
 
 /** Model -> User > Promise */
 const register = (model: Model<Document>) => (user: User) => model
@@ -14,8 +31,15 @@ const register = (model: Model<Document>) => (user: User) => model
     if (doc != null) {
       throw new Error(`User with email "${ user.email }" already exists`)
     } else {
-      return model.create(user);
+      return registerUser(model)(user);
     }
+  })
+  .then(doc => new Promise((resolve, reject) => doc.validate(err =>
+    err == null ? resolve(doc) : reject(err))))
+  .then(doc => userDocumentToObject(doc))
+  .catch(err => {
+    console.error(err);
+    throw err;
   });
 
 /** Model -> Credentials -> Promise */
@@ -27,23 +51,18 @@ const login = (model: Model<Document>) => credentials => model
       throw new Error(`User with email "${ credentials.email }" doesn't exists`);
     }
 
-    strictEqual(doc.password, credentials.password, 'Incorrect password');
-
-    return doc.toObject({
-      minimize: true,
-      transform: (doc, ret) => {
-        delete (ret as any).password;
-      }
-    });
+    return compare(credentials.password, doc.password)
+      .then(isMatch => {
+        if (isMatch) {
+          return userDocumentToObject(doc);
+        } else {
+          throw new Error('Incorrect password');
+        }
+      });
   });
 
-export interface AuthControllerMethods {
-  login: RequestHandler,
-  register: RequestHandler
-}
-
 /** Connection -> Model -> AuthControllerMethods */
-export const getAuthController = (connection: Connection): AuthControllerMethods => {
+export const getAuthController = (connection: Connection): ControllerMethods => {
   const UserModel = getUserModel(connection);
 
   return {
